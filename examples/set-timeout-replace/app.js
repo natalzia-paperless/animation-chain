9881d55d74bbf796988f5032532835ed681ce416
Joe Natalzia <jnatalzia@gmail.com>
2015-02-21 17:21:30 -0500

    Update example for timeouts

--- examples/set-timeout-replace/app.js
+++ examples/set-timeout-replace/app.js
@@ -1,8 +1,26 @@
 var chain = require('animation-chain');
 
-chain({
-  callback: function() {
-    console.log('testing');
-  },
-  time: 400
-});
+document.querySelector('button').onclick = function() {
+  var selectors = ["val1","val2","num1","num2"];
+
+  var valueObject = {};
+
+  for (var i = 0; i < selectors.length; i++) {
+    var sel = selectors[i];
+    valueObject[sel] = document.querySelector("."+sel).value;
+  }
+
+  var obj1 = {
+    callback: function() {
+      console.log(valueObject.val1);
+    },
+    time: valueObject.num1
+  }, obj2 = {
+    callback: function() {
+      console.log(valueObject.val2);
+    },
+    time: valueObject.num2
+  }
+
+  chain(obj1).chainTo(obj2);
+}
